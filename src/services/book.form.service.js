b930929e16fd521b03f98714693c139114ebe532
warrenwwc <wwwc@outlook.com>
2019-02-19 07:56:29 +0800

    Changes airport sorting. Fixes AB#3464.

--- src/services/book.form.service.js
+++ src/services/book.form.service.js
@@ -17,6 +17,11 @@ class BookFormService {
  airports: this._airports.getAll()
         };
     }
+
+    getAll(){
+        return this._airports.filter(a => a.code).map(avoidEmptyCity).sort((a, b) => (a.city > b.city) ? 1 : -1);
+    }
+
 }
 
 module.exports = BookFormService;
