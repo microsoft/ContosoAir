f517e50f116607c6dcff73fdb1a41d0adaffa451
Sebass van Boxel <svboxel@gmail.com>
2019-02-18 16:48:55 +0100

    Update book.form.service.js

--- src/services/book.form.service.js
+++ src/services/book.form.service.js
@@ -14,7 +14,7 @@ class BookFormService {
             ],
             today: moment().toDate(),
             passengers: [1, 2, 3, 4, 5],
-            airports: this._airports.getAll().sort((first, second) => first.city.localeCompare(second.city))
+            airports: this._airports.getAll()
         };
     }
 }
