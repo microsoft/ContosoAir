9154d74324ac296e781f8faf0a38fa56d3de9801
Sachin Raj <h.sachin@outlook.com>
2019-03-13 16:31:08 -0700

    Update airports.service.js

--- src/services/airports.service.js
+++ src/services/airports.service.js
@@ -8,8 +8,7 @@ class AirportsService {
     }
 
     getAll(){
-        //changing sort order
-      return this._airports.filter(a => a.code).map(avoidEmptyCity).sort((a, b) => (a.city > b.city) ? 1 : -1);
+        return this._airports.filter(a => a.code).map(avoidEmptyCity).sort((a, b) => (a.city > b.city) ? 1 : -1);
     }
 
     getByCode(code) {
