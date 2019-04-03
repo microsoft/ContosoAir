2bc2a0040a22c9f9d45920a55404b5b1975c151d
warrenwwc <wwwc@outlook.com>
2019-04-03 18:41:14 +0800

    reset

--- src/services/airports.service.js
+++ src/services/airports.service.js
@@ -11,6 +11,10 @@ class AirportsService {
         return this._airports.filter(a => a.code).map(avoidEmptyCity);
     }
 
+    // getAll(){
+    //     return this._airports.filter(a => a.code).map(avoidEmptyCity).sort((a, b) => (a.city > b.city) ? 1 : -1);
+    // }
+
     getByCode(code) {
         return avoidEmptyCity(this._airports.find(a => a.code == code));
     }
