411198fee927e0eb1b2ac73aa9b372920ba8d002
warrenwwc <wwwc@outlook.com>
2019-04-04 10:41:25 +0800

    Fixes AB#3

--- src/services/airports.service.js
+++ src/services/airports.service.js
@@ -7,13 +7,13 @@ class AirportsService {
         this._airports = airports;
     }
 
-    getAll(){
-        return this._airports.filter(a => a.code).map(avoidEmptyCity);
-    }
-
     // getAll(){
-    //     return this._airports.filter(a => a.code).map(avoidEmptyCity).sort((a, b) => (a.city > b.city) ? 1 : -1);
+    //     return this._airports.filter(a => a.code).map(avoidEmptyCity);
     // }
+
+    getAll(){
+        return this._airports.filter(a => a.code).map(avoidEmptyCity).sort((a, b) => (a.city > b.city) ? 1 : -1);
+    }
 
     getByCode(code) {
         return avoidEmptyCity(this._airports.find(a => a.code == code));
