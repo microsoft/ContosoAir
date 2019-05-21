2752c7c4bdde84d6c5bc9158c2761a566f210922
Jim Vacca <jamesvac@microsoft.com>
2019-05-21 12:49:24 -0400

    Changes Airport Sorting. fixes AB#276

--- src/services/airports.service.js
+++ src/services/airports.service.js
@@ -5,11 +5,11 @@ const avoidEmptyCity = function(a) {
 class AirportsService {
     constructor(airports){
         this._airports = airports;
-    }
+    } 
 
     getAll(){
-        return this._airports.filter(a => a.code).map(avoidEmptyCity);
-    }
+            return this._airports.filter(a => a.code).map(avoidEmptyCity).sort((a, b) => (a.city > b.city) ? 1 : -1);
+        }
 
     getByCode(code) {
         return avoidEmptyCity(this._airports.find(a => a.code == code));
