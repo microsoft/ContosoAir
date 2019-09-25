9cc23d676f3cfd58fd11f6fc810d840761bd4186
Gerwald Oberleitner <gerwaldo@users.noreply.github.com>
2019-09-25 17:06:28 +0200

    AB#558
    AB#558

--- src/services/airports.service.js
+++ src/services/airports.service.js
@@ -8,8 +8,8 @@ class AirportsService {
     }
 
     getAll(){
-        return this._airports.filter(a => a.code).map(avoidEmptyCity);
-        // return this._airports.filter(a => a.code).map(avoidEmptyCity).sort((a, b) => (a.city > b.city) ? 1 : -1);
+        // return this._airports.filter(a => a.code).map(avoidEmptyCity);
+        return this._airports.filter(a => a.code).map(avoidEmptyCity).sort((a, b) => (a.city > b.city) ? 1 : -1);
         
         // Integration with AB#123 for Azure Boards
     }
